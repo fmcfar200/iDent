9c850669f15cab1b7344b1130aa94a130e3a683b
fmcfar200 <fmcfar200@caledonian.ac.uk>
2019-03-13 18:14:03 +0000

    Javascript fix

--- iDent/Views/ApplyPageReview.xaml.cs
+++ iDent/Views/ApplyPageReview.xaml.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web;
+
 
 using Xamarin.Forms;
 using Xamarin.Forms.Xaml;
@@ -20,33 +22,32 @@ namespace iDent.Views
 		{
 			InitializeComponent ();
             applicationForm = applyForm;
-            script = "javascript:" + //$"document.getElementById('multichoice_23').value = {applicationForm.CourseNumber};" +
-            //$"document.getElementById('textfield_24').value = {applicationForm.DateOfApplication.ToString()};" +
-            $"document.getElementById('textfield_6').value = fraser;";
-           //$"document.getElementById('textarea_8').value = {applicationForm.Address};" +
-           //$"document.getElementById('textfield_9').value = {applicationForm.DOB};" +
-           //$"document.getElementById('textfield_7').value = {applicationForm.email};" +
-           //$"document.getElementById('textfield_10').value = {applicationForm.HomeNumber};" +
-           //$"document.getElementById('textfield_11').value = {applicationForm.MobileNumber};" +
-           //$"document.getElementById('textarea_20').value = {applicationForm.SchoolCollege};" +
-           //$"document.getElementById('textarea_22').value = {applicationForm.Qualifications};" +
-           //$"document.getElementById('textfield_13').value = {applicationForm.SCN};" +
-           //$"document.getElementById('textarea_18').value = {applicationForm.ReasonForApplication};" +
-           //$"document.getElementById('textfield_19').value = {applicationForm.WhereDidYouHear};" +
-           //$"document.getElementById('textfield_15').value = {applicationForm.EmployerName};" +
-           //$"document.getElementById('textarea_16').value = {applicationForm.EmployerAddress};" +
-           //$"document.getElementById('textfield_17').value = {applicationForm.EmployerNumber};";
+            script = "javascript:" + $"document.getElementById('multichoice_23').value = {applicationForm.CourseNumber};" +
+            $"document.getElementById('textfield_24').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.DateOfApplication)}';" +
+            $"document.getElementById('textfield_6').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.Name)}';"+
+            $"document.getElementById('textarea_8').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.Address)}';" +
+            $"document.getElementById('textfield_9').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.DOB)}';" +
+            $"document.getElementById('textfield_7').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.email)}';" +
+            $"document.getElementById('textfield_10').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.HomeNumber)}';" +
+            $"document.getElementById('textfield_11').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.MobileNumber)}';" +
+            $"document.getElementById('textarea_20').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.SchoolCollege)}';" +
+            $"document.getElementById('textarea_22').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.Qualifications)}';" +
+            $"document.getElementById('textfield_13').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.SCN)}';" +
+            $"document.getElementById('textarea_18').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.ReasonForApplication)}';" +
+            $"document.getElementById('textfield_19').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.WhereDidYouHear)}';" +
+            $"document.getElementById('textfield_15').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.EmployerName)}';" +
+            $"document.getElementById('textarea_16').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.EmployerAddress)}';" +
+            $"document.getElementById('textfield_17').value = '{HttpUtility.JavaScriptStringEncode(applicationForm.EmployerNumber)}';";
 
 
 
 
         }
 
-        private void Button_ClickedAsync(object sender, EventArgs e)
+        private async void Button_ClickedAsync(object sender, EventArgs e)
         {
-            Task.Run(async () =>
-            {
-            });
+                await _webView.EvaluateJavaScriptAsync(script);
+
         }
 
 
